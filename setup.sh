3fa90b9519491f3d5420fe9e4e521b1cbf30a9ab
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-03-11 11:19:58 -0400

    Added sublime config.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+# Bash public config
 cd ~
 ln -s ~/.dotfiles/.bash_public .
 
-
+# Sublime config
+cd ~/.config/sublime-text-3/Packages/User/
+ln -s ~/.dotfiles/sublime-text-3/external .
+ln -s ~/.dotfiles/sublime-text-3/Preferences.sublime-settings .
